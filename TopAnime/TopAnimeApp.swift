1afbf1b0b07b9561b2895b06bb60fa9febc3ac81
yakymchukwork9030-dev <yakymchukwork9030@gmail.com>
2026-05-29 17:28:24 +0300

    Initial Commit

---
+++ TopAnime/TopAnimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopAnimeApp.swift
+//  TopAnime
+//
+//  Created by Maksym Yakymchuk on 29.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct TopAnimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
